85e6d9836d240bb062d40e23311c460a34679826
Kaegan O'Hara <kohara321@gmail.com>
2022-05-21 16:27:15 -0600

    commit tree ad0a544622592afe817e7b7ef3e7a904ffa2d32e

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 function addingEventListener() {
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+}
+
+
+// const inputBox = document.getElementById('input');
+// const clickAlert = function clickAlert(){
+//     alert('Hee hee, that tickles!!')
+// }
+
+// input.addEventListener('click', clickAlert)
+
+// const mainTitle = document.getElementById('main');
+
+// mainTitle.addEventListener('click', clickAlert)
